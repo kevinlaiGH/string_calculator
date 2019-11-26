fc420777be494a7097bb9d263c72eefd28c86c90
Kevin Lai <kevin.km.lai@gmail.com>
2019-11-27 02:19:28 +1100

    passed 10,10,10 test 🚀

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const add = (input) => {
         return 0
     } else if (input.includes(",")) {
         const parts = input.split(",")
-        const num1 = parseInt(parts[0], 10)
-        const num2 = parseInt(parts[1], 10)
+        // const num1 = parseInt(parts[0], 10)
+        // const num2 = parseInt(parts[1], 10)
 
+        let temp = []
 
-
-        return num1 + num2
+        for ( let i = 0; i < parts.length; i++){
+            temp.push(parseInt(parts[i], 10))
+        }
+        return temp.reduce(( a, b ) => a + b , 0)
+        //
+        // return num1 + num2
     }else {
         return parseInt(input,10)
     }
