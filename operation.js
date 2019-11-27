906a26854bdb59b2c2f94c0b8a5ae8142aff5d38
Kevin Lai <kevin.km.lai@gmail.com>
2019-11-27 15:39:09 +1100

    changed

--- operation.js
+++ operation.js
@@ -17,24 +17,11 @@ const add = (input) => {
     }
 }
 
-const subtract = (input) => {
-    if (input === "" || input === null){
-        return 0
-    } else if ( input.includes("-")){
-        const parts = input.split("-")
-        let temp = []
-        for ( let i = 0; i < parts.length; i ++){
-            temp.push(parseInt(parts[i], 10))
-        }
-        return temp.reduce((a,b) => a - b)
-    }else {
-        return parseInt(input, 10)
-    }
-}
-//pass tested 100-10-10
+
+
 
 
 module.exports = {
     add,
-    subtract
+
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const add = require('../operation.js').add;
 const subtract = require('../operation.js').subtract;
+const divide = require('../operation.js').divide;
 
 describe('#add()', function () {
     it('should return 0 when empty string is passed', function () {
@@ -48,21 +49,12 @@ describe('#add()', function () {
         // Then
         assert.equal(result, expected)
     });
-    it('should return 90 when "100-10" are passed', function () {
+    it('should return 30 when "10 10 10" are passed', function () {
         // Given
-        const input = "100-10"
-        const expected = 90
+        const input = "10 10 10"
+        const expected = 30
         // When
-        const result = subtract(input)
-        // Then
-        assert.equal(result, expected)
-    });
-    it('should return 80 when "100-10-10" are passed', function () {
-        // Given
-        const input = "100-10-10"
-        const expected = 80
-        // When
-        const result = subtract(input)
+        const result = add(input)
         // Then
         assert.equal(result, expected)
     });
