6ee5a51c2478959e5e5260ee7bfab7e6b2b7897b
Kevin Lai <kevin.km.lai@gmail.com>
2019-12-03 20:47:59 +1100

    deleted some code

--- operation.js
+++ operation.js
@@ -4,33 +4,13 @@
 const check2 = RegExp(/^(..|[0-9,])/)
 // also need to take in consideration of \s whitespaces
 // it means any number, 1 or more characters
-
 const numbersOnlyCheck = RegExp(/[0-9]+/)
-
+// https://regex101.com/
 const numbersAndSymbolsCheck = RegExp(/(\d)+/g)
 
 const summation =  (splittedArr) =>{
-
     let intArray = splittedArr.map(x=> parseInt(x, 10))
-
     return intArray.reduce((a, b) => a + b, 0)
-}
-
-const gimmeAnswer = (parts) => {
-    let newArr = []
-    for (let i = 0; i < parts.length; i++) {
-        if (parts[i].includes("..")) {
-            let sumOfConsecutiveNum = parts[i].split("..")
-            let x = parseInt(sumOfConsecutiveNum[0], 10)
-            let y = parseInt(sumOfConsecutiveNum[1], 10)
-            let answer = (x + y) * (y - x + 1) / 2
-            newArr.push(answer)
-        } else {
-            let z = parseInt(parts[i], 10)
-            newArr.push(z)
-        }
-    }
-    return newArr.reduce((a, b) => a + b, 0)
 }
 
 const gimmeAnswer2 = (parts) => {
@@ -88,9 +68,6 @@ const add = (input) => {
     return summation(partBWithoutCheck2)
 }
 
-const yeah = add("10,10,10" )
-// const yeah = add("3,4..6,7,10,33..36,1"  )
-// const yeah = add("3,4"  )
 
 module.exports = {
     add,
