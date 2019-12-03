eb4a2919913b85b0e05126f957603eebd1f86434
Kevin Lai <kevin.km.lai@gmail.com>
2019-12-03 20:31:38 +1100

    test passed after refactoring

---
+++ howToUseMap.js
@@ -0,0 +1,10 @@
+["1","2","3","4","5"].map((value) => parseInt(value)).reduce((a,b)=> a+b,0)
+
+
+
+// let temp = []
+//
+// for ( let i = 0; i < parts.length; i++){
+//     temp.push(parseInt(parts[i], 10))
+// }
+// return temp.reduce(( a, b ) => a + b , 0)
--- operation.js
+++ operation.js
@@ -1,54 +1,96 @@
 // https://stackoverflow.com/questions/8359566/regex-to-match-symbols
 
 // Changed: excluded/ deleted comma and dot
-const checker = RegExp(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/\s\n]/)
+const check2 = RegExp(/^(..|[0-9,])/)
+// also need to take in consideration of \s whitespaces
+// it means any number, 1 or more characters
 
-const check2 = RegExp(/[-!$%^&*()_+|~=`{}\[\]:";'<>?\/\s\n]/)
+const numbersOnlyCheck = RegExp(/[0-9]+/)
 
-const add = (input) => {
-    if (input === "" || input === null) {
-        return 0
-    }
-    if (checker.test(input)) {
-        let newArr =[]
-        // if it has commas, we will split it first and deal with .. later
-        if (input.indexOf(',') > -1) {
-            const parts = input.split(",")
+const numbersAndSymbolsCheck = RegExp(/(\d)+/g)
 
-            for (let i=0 ; i< parts.length; i++){
-                if (parts[i].includes("..")){
-                    let sumOfConsecutiveNum = parts[i].split("..")
-                    let x = parseInt(sumOfConsecutiveNum[0],10)
-                    let y = parseInt(sumOfConsecutiveNum[1],10)
-                    let answer =(x+y)*(y-x+1)/2
-                    newArr.push(answer)
-                }
-                else {
-                    let z = parseInt(parts[i],10)
-                    newArr.push(z)
-                }
-            }
-            return newArr.reduce(( a, b ) => a + b , 0)
-            // console.log("hello here:",newArr.reduce(( a, b ) => a + b , 0))
-        }
+const summation =  (splittedArr) =>{
 
-        // if it doesn't has commas or dots, we will split it
-        if (check2.test(input)){
-            const partB = input.split(check2)
-            const partBWithoutCheck2 = partB.filter(x => x !== '')
-            // console.log("partBWithoutCheck2: ",partBWithoutCheck2 )
-            let temp = []
+    let intArray = splittedArr.map(x=> parseInt(x, 10))
 
-            for ( let i = 0; i < partBWithoutCheck2 .length; i++){
-                temp.push(parseInt(partBWithoutCheck2 [i], 10))
-            }
-            return temp.reduce(( a, b ) => a + b , 0)
-        }
-        return parseInt(input,10)
-    }
-    return parseInt(input,10)
+    return intArray.reduce((a, b) => a + b, 0)
 }
 
+const gimmeAnswer = (parts) => {
+    let newArr = []
+    for (let i = 0; i < parts.length; i++) {
+        if (parts[i].includes("..")) {
+            let sumOfConsecutiveNum = parts[i].split("..")
+            let x = parseInt(sumOfConsecutiveNum[0], 10)
+            let y = parseInt(sumOfConsecutiveNum[1], 10)
+            let answer = (x + y) * (y - x + 1) / 2
+            newArr.push(answer)
+        } else {
+            let z = parseInt(parts[i], 10)
+            newArr.push(z)
+        }
+    }
+    return newArr.reduce((a, b) => a + b, 0)
+}
+
+const gimmeAnswer2 = (parts) => {
+    let arr = parts.map(function(val) {
+        if (val.includes("..")) {
+            let sumOfConsecutiveNum = val.split("..")
+            let x = parseInt(sumOfConsecutiveNum[0], 10)
+            let y = parseInt(sumOfConsecutiveNum[1], 10)
+            let answer = (x + y) * (y - x + 1) / 2
+            return answer
+        } else {
+            let z = parseInt(val, 10)
+            return z
+        }
+    })
+
+    return arr.reduce((a, b) => a + b, 0)
+
+}
+
+const add = (input) => {
+    if ("" === input || null === input) {
+        return 0
+    }
+    if (input.includes("..") && input.includes(",")) {
+        // if it has commas, we will split it first and deal with .. later
+        const parts = input.split(",")
+        return gimmeAnswer2(parts)
+
+    }
+    if (input.includes(",")){
+        // if it has commas, we will split it first and deal with .. later
+        const partss = input.split(",")
+        return gimmeAnswer2(partss)
+    }
+
+    if (input.includes(" ")) {
+        const partsC = input.split(" ")
+        return summation(partsC)
+    }
+
+    if (numbersAndSymbolsCheck.test(input)) {
+        let found = input.match(numbersAndSymbolsCheck)
+        return summation(found)
+    }
+
+    // if only integer as input, we output integer only
+    if (numbersOnlyCheck.test(input)) {
+        return parseInt(input, 10)
+    }
+
+    // if it doesn't has commas or dots, we will split it
+    const partB = input.split(check2)
+    const partBWithoutCheck2 = partB.filter(x => x !== '')
+    return summation(partBWithoutCheck2)
+}
+
+const yeah = add("10,10,10" )
+// const yeah = add("3,4..6,7,10,33..36,1"  )
+// const yeah = add("3,4"  )
 
 module.exports = {
     add,
